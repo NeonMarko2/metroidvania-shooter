608466194752432419a221de27e72c9d795d6b3f
Marko <neonmarko2@gmail.com>
2025-10-14 11:34:08 +0200

    Testing github, added test horizontal movement

--- player.lua
+++ player.lua
@@ -2,7 +2,13 @@ local player = {}
 
 player.position = Vector2.new(200, 200)
 
-function player:update(dt) end
+function player:update(dt)
+	if love.keyboard.isDown("d") then
+		self.position.x = self.position.x + 25 * dt
+	elseif love.keyboard.isDown("a") then
+		self.position.x = self.position.x - 25 * dt
+	end
+end
 
 function player:draw()
 	love.graphics.rectangle("fill", self.position.x, self.position.y, 50, 50)
