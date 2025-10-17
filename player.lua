90b4e2f9c5a29bccca0e5abfb10e8b259979978a
Marko <neonmarko2@gmail.com>
2025-10-17 09:58:05 +0200

    added collider and body to player, made simple jumping

--- player.lua
+++ player.lua
@@ -1,17 +1,26 @@
 local player = {}
 
-player.position = Vector2.new(200, 200)
+player.position = Vector2.new(200, 300)
+player.collider = Collision:addCollider(player.position, Vector2.new(50, 50), false)
+player.body = PhysicsBody.new(player.collider)
 
 function player:update(dt)
 	if love.keyboard.isDown("d") then
-		self.position.x = self.position.x + 25 * dt
+		player.body:move(self.position + Vector2.new(180, 0) * dt)
 	elseif love.keyboard.isDown("a") then
-		self.position.x = self.position.x - 25 * dt
+		player.body:move(self.position - Vector2.new(180, 0) * dt)
 	end
+	if love.keyboard.isDown("space") then
+		player.body:throw(Vector2.new(0, -100))
+	end
+	player.body:update(dt)
+	player.position = player.collider.position
 end
 
+function player:move(newPosition) end
+
 function player:draw()
-	love.graphics.rectangle("fill", self.position.x, self.position.y, 50, 50)
+	love.graphics.rectangle("fill", self.position.x - 25, self.position.y - 25, 50, 50)
 end
 
 return player
